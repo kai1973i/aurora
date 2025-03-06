cb2ad121013f86f21b3382f43f6814594eea4a05
kai1973i <kai1973i@googlemail.com>
2025-03-06 22:45:30 +0100

    add network host

--- build_image.sh
+++ build_image.sh
@@ -5,7 +5,7 @@ docker build -t aurora-app . --network=host
 
 echo "Docker-Image wurde erfolgreich erstellt."
 
-docker run -d -p 5173:5173 --name aurora-container aurora-app
+docker run -d -p 5173:5173 --name aurora-container aurora-app --network=host
 
 echo "Docker-Container wurde erfolgreich gestartet."
 
